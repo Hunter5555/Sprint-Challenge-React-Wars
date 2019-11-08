606aa2a6546262c7afa9e6eba649eaf06ec88414
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-08 11:52:09 -0700

    mvp

--- starwars/src/components/Person.js
+++ starwars/src/components/Person.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const PersonCard = (props) =>{
-    console.log(props.hair_color)
+const StyledCard = styled.div`
+    background: black;
+    width: 400px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    opacity: 0.5;
+    color: yellow;
+    box-shadow: 0 8px 6px -6px black;
+    font-size: 1.4rem;
+`;
+
+const PersonCard = (props) => {
+   console.log(props.hair_color)
     return(
         <StyledCard>
             <h2>Name: {props.name}</h2>
             <p>Gender: {props.gender}</p>
-            <p>Height: {props.height}</p>
-            <p>Mass: {props.mass}</p>
-            <p>Hair Color: {props.hari_color}</p>
+            <p>Height: {props.height}cm</p>
+            <p>Mass: {props.mass}kg</p>
+            <p>Hair Color: {props.hair_color}</p>
         </StyledCard>
     )
-}
-export default PersonCard;
+} 
+export default PersonCard; 
